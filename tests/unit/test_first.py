99f859cd95e4cc79fc54a30f81a38b676b2c4713
AlexTraveylan <monsieurdemares@gmail.com>
2023-12-01 07:12:59 +0100

    premier jour

--- tests/unit/test_first.py
+++ tests/unit/test_first.py
@@ -1,4 +1,4 @@
-from app.first.code import sum_calibration_values_v2
+from app.first.code import sum_calibration_values, sum_calibration_values_v2
 import pytest
 
 
@@ -14,5 +14,18 @@ import pytest
         (["7pqrstsixteen"], 76),
     ],
 )
-def test_01_12_20223(input, expected):
+def test_01_12_2023_v2(input, expected):
     assert sum_calibration_values_v2(input) == expected
+
+
+@pytest.mark.parametrize(
+    "input, expected",
+    [
+        (["1abc2"], 12),
+        (["pqr3stu8vwx"], 38),
+        (["a1b2c3d4e5f"], 15),
+        (["treb7uchet"], 77),
+    ],
+)
+def test_01_12_2023_v1(input, expected):
+    assert sum_calibration_values(input) == expected
